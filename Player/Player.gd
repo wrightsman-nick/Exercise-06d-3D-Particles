8b1b2db63700ec09d9a8fa6b26d782b3e7c89b77
wrightsman-nick <77906618+wrightsman-nick@users.noreply.github.com>
2021-04-28 20:41:38 -0400

    had a lovely time adding particles

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,6 +1,8 @@
 extends KinematicBody
 
-onready var Camera = $Pivot/Cameraz
+onready var Camera = $Pivot/Camera
+onready var Explosion = load("res://Explosion/Explosion.tscn")
+onready var Explosions = get_node("/root/Game/Explosions")
 
 var gravity = -30
 var max_speed = 8
@@ -23,4 +25,7 @@ func _physics_process(_delta):
 		if $Pivot/RayCast.is_colliding():
 			var target = $Pivot/RayCast.get_collider()
 			if target.is_in_group("target"):
+				var explosion = Explosion.instance()
+				Explosions.add_child(explosion)
+				explosion.global_transform.origin = $Pivot/RayCast.get_collision_point()
 				target.die()
